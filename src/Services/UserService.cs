5e071adcbe87c97e5450bcab26719f72eb368ccb
Turkiano <turki.saeed.ksa@gmail.com>
2024-05-28 12:18:31 +0300

    rreay to deploy

--- src/Services/UserService.cs
+++ src/Services/UserService.cs
@@ -31,7 +31,7 @@ public class UserService : IUserService
         User? user = _userRepository.FindOneByEmail(userSign.Email);
         if (user is null) return null;
 
-        byte[] pepper = Encoding.UTF8.GetBytes(_config["Jwt:Pepper"]!);
+        byte[] pepper = Encoding.UTF8.GetBytes(_config["jwt_Pepper"]!);
 
         bool isCorrectPass = PasswordUtils.VerifyPassword(userSign.Password, user.Password, pepper);
         if (!isCorrectPass) return null;
@@ -43,12 +43,12 @@ public class UserService : IUserService
             new Claim(ClaimTypes.Email, user.Email),
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
         };
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SigningKey"]!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["jwt_SigningKey"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
-            issuer: _config["Jwt:Issuer"],
-            audience: _config["Jwt:Audience"],
+            issuer: _config["jwt_Issuer"],
+            audience: _config["jwt_Audience"],
             claims: claims,
             expires: DateTime.Now.AddDays(7),
             signingCredentials: creds
@@ -67,7 +67,7 @@ public class UserService : IUserService
             return null;
         }
 
-        byte[] pepper = Encoding.UTF8.GetBytes(_config["Jwt:Pepper"]!);
+        byte[] pepper = Encoding.UTF8.GetBytes(_config["jwt_Pepper"]!);
 
         PasswordUtils.HashPassword(user.Password, out string hashedPassword, pepper);
 
